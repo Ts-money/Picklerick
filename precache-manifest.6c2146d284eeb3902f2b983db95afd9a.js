82d814eeb829a3bd14d2b46d6bab637be65bfb15
Talib Shaikh <talibnisharshaikh@gmail.com>
2019-12-30 13:14:53 -0500

    Updates

---
+++ precache-manifest.6c2146d284eeb3902f2b983db95afd9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/Picklerick/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "4b4420cffe3ba4f13221e529e5b60078",
+    "url": "/Picklerick/static/media/get_schwifty.4b4420cf.ttf"
+  },
+  {
+    "revision": "59dac4697b87529021cc",
+    "url": "/Picklerick/static/js/runtime~main.041e51af.js"
+  },
+  {
+    "revision": "e3b1936d3b6c6fe17bed",
+    "url": "/Picklerick/static/js/main.62af12af.chunk.js"
+  },
+  {
+    "revision": "4a51e64ee84228e42254",
+    "url": "/Picklerick/static/js/2.f707080e.chunk.js"
+  },
+  {
+    "revision": "e3b1936d3b6c6fe17bed",
+    "url": "/Picklerick/static/css/main.50336d78.chunk.css"
+  },
+  {
+    "revision": "09af38ba82075fa49c145482ca90a39e",
+    "url": "/Picklerick/index.html"
+  }
+];
